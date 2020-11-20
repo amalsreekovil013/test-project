7e34f1ad8df799e5432aa195393e56d96958e380
amalsreekovil013 <74757597+amalsreekovil013@users.noreply.github.com>
2020-11-20 11:31:22 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb75557b83a2f001f2e1b7d",
+				"5fb75557b83a2f001f2e1b7f"
+			]
+		},
+		"5fb75557b83a2f001f2e1b7d": {
+			"id": "5fb75557b83a2f001f2e1b7d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb75557b83a2f001f2e1b7f": {
+			"id": "5fb75557b83a2f001f2e1b7f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
